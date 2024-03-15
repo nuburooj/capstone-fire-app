7fc00b16898d603499e048a76c69ca2b5f49b931
nuburooj <nuburoojkhattak@gmail.com>
2024-03-15 15:45:17 -0400

    created signup component and added route to app.js. its working,  bout to jump into layout and functionality

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,8 +1,21 @@
 import React, { useEffect, useState } from "react";
-import { Switch, Route } from "react-router-dom";
+import { Route, BrowserRouter, Routes } from "react-router-dom";
+
+import UserSignup from "./user_components/UserSignup";
 
 function App() {
-  return <h1>Project Client</h1>;
+  return (
+    <div>
+      <div classname="App">
+        <BrowserRouter>
+          <Routes>
+            <Route path='/signup' element={<UserSignup />} />
+          </Routes>
+        </BrowserRouter>
+
+      </div>
+    </div>
+  )
 }
 
 export default App;
---
+++ client/src/components/user_components/UserSignup.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Formik, Form, useField } from 'formik';
+import * as Yup from 'yup';
+import { useNavigate } from 'react-router-dom';
+
+
+function UserSignup(){
+    return <h1>User Signup</h1>;
+}
+
+export default UserSignup;
