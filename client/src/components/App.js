38f90e75fe21067ca0754f20e8f1899820f9e31f
nuburooj <nuburoojkhattak@gmail.com>
2024-03-17 03:31:58 -0400

    made user context componrnt and wrapped routes in app with contextProvider, added functionality to login page, tested with thunder client and user is able to login but check session returning error

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -3,19 +3,23 @@ import { Route, BrowserRouter, Routes } from "react-router-dom";
 
 import UserSignup from "./user_components/UserSignup";
 import UserLogin from "./user_components/UserLogin";
+import UserContext from "./user_components/UserContext";
 
 function App() {
+  const [currentUser, setCurrentUser] = useState({})
+
   return (
     <div>
-      <div classname="App">
-        <BrowserRouter>
-          <Routes>
-            <Route path='/signup' element={<UserSignup />} />
-            <Route path='/login' element={<UserLogin />} />
-          </Routes>
-        </BrowserRouter>
-
-      </div>
+      <UserContext.Provider value={{currentUser, setCurrentUser}}>
+        <div classname="App">
+          <BrowserRouter>
+            <Routes>
+              <Route path='/signup' element={<UserSignup />} />
+              <Route path='/login' element={<UserLogin />} />
+            </Routes>
+          </BrowserRouter>
+        </div>
+      </UserContext.Provider>
     </div>
   )
 }
--- client/src/components/user_components/UserLogin.jsx
+++ client/src/components/user_components/UserLogin.jsx
@@ -4,9 +4,63 @@ import * as Yup from 'yup';
 import { useNavigate } from 'react-router-dom';
 
 function UserLogin() {
+
+    const navigate = useNavigate()
+
+     const LoginTextInput = ({lable, ...props}) => {
+        
+        const [field, meta] = useField(props)
+        return(
+            <div className='form-group'>
+                <lable htmlFor={props.id || props.name}>{lable}</lable>
+                <input className='text-input' {...field} {...props} />
+                {meta.touched && meta.error ? (
+                    <div className='error'>{meta.error}</div>
+                ) : null}
+            </div>
+        )
+    }
+
     return(
-        <div>
+        <div className='login-heading'>
             <h1>Login</h1>
+            <div> 
+                <Formik
+                    initialValues={{
+                        username: "",
+                        password: ""
+                    }}
+                    validationSchema={Yup.object({
+                        username: Yup.string()
+                        .required('Username is required.'),
+                        password: Yup.string()
+                        .required('Password is required')
+                    })}
+                    onSubmit={(values, { setSubitting, resetForm }) => {
+                        
+                        fetch(`http://localhost:5555/login`, {
+                            method: 'POST',
+                            headers: {
+                                'Content-Type': 'application/json'
+                            },
+                            body: JSON.stringify(values)    
+                        })
+                        .then(res => res.json())
+                        .then(values => {
+                            console.log(values)
+                            navigate('/')
+                        })
+                        .then( setSubitting(false), resetForm() );
+                    }}
+
+                    >
+                        <Form className='login-Form'>
+                            <LoginTextInput type="text" name="username" lable="Username" />
+                            <LoginTextInput type="password" name="password" lable="Password" />
+                            <button type="submit">Login</button>
+                        </Form>
+                </Formik>
+            </div>
         </div>
     )
 
