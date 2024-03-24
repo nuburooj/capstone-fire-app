83c4764ea01f245d67cea00937b55316694c66bf
nuburooj <nuburoojkhattak@gmail.com>
2024-03-24 03:12:51 -0400

    made comments popup on homepage songposts.

--- client/src/components/home_components/SongObject.jsx
+++ client/src/components/home_components/SongObject.jsx
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom'
 import './SongStyles.css';
 import AudioFile from '../home_components/audio.mp3';
 import AudioPlayerHome from '../Player_components/AudioPlayerHome';
+import CreateComment from '../comment_components/CreateComment';
 
 function SongObject({
     id,
@@ -14,6 +15,40 @@ function SongObject({
     genre_id
 }){
 
+    const [currentSong, setCurrentSong] = useState({
+        song_title: '',
+        song_description: '',
+        song_artwork: '',
+        upload_file: ''
+    });
+    console.log(currentSong)
+   
+
+
+    function handleNewComment(newComment) {
+        console.log(newComment); 
+        if (newComment && newComment.id) {
+            setCurrentSong(prevSong => ({
+                ...prevSong,
+                comments: [...prevSong.comments, newComment]
+            }));
+        } else {
+            console.error('Attempted to add an undefined or invalid Comment:', newComment);
+        }
+    };
+
+
+    useEffect(() => {
+        fetch(`http://localhost:5555/songs/${id}`)
+            .then(response => response.json())
+            .then(data => {
+                console.log(data.comments)
+                setCurrentSong(prevState => ({
+                    ...prevState, 
+                    ...data 
+                }));
+            });
+    }, [id]);
 
     return(
       <div>
@@ -25,6 +60,16 @@ function SongObject({
                 {upload_file ? <AudioPlayerHome audioFile={upload_file} /> : "Loading..."}
                 {upload_file && <p className="song-link"><a href={upload_file} target="_blank" rel="noopener noreferrer">Download/View File</a></p>}
                 <p className="song-description">description: {song_description}</p>
+                <div>
+                    <div>
+                        {currentSong.comments && currentSong.comments.map(comment => (
+                            <div key={comment.id}>
+                                <p>{comment.comment_description}</p>
+                            </div>
+                        ))}
+                    </div>
+                    <CreateComment onAddComment={handleNewComment} songId={id} />
+                </div>
             </div>
         </div>
     )
